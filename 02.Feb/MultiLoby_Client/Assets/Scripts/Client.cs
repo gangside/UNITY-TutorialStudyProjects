35bfa70870ef6e0c500ad70f559273076daa5299
gangside <x_hojun@naver.com>
2020-02-03 23:51:07 +0900

    Start multi loby tutorial, using Unet

---
+++ 02.Feb/MultiLoby_Client/Assets/Scripts/Client.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Client : MonoBehaviour {
+
+	const int MAX_USER = 100;
+	const int PORT = 26000;
+	const int WEB_PORT = 26001;
+	const string SERVER_IP = "127.0.0.1";
+
+	byte reliableChannel;
+	int hostId;
+	byte error;
+	bool isStarted = false;
+
+	#region Monobehaviour
+	void Start() {
+		DontDestroyOnLoad(this.gameObject);
+		Init();
+	}
+	#endregion
+
+	public void Init() {
+		NetworkTransport.Init();
+
+		ConnectionConfig cc = new ConnectionConfig();
+		reliableChannel = cc.AddChannel(QosType.Reliable);
+
+		HostTopology topo = new HostTopology(cc, MAX_USER);
+
+		//server only code
+		hostId = NetworkTransport.AddHost(topo, 0);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+		//web client
+		NetworkTransport.Connect(hostId, SERVER_IP, WEB_PORT, 0, out error);
+		Debug.Log(string.Format("Connecting from Web"));
+#else
+		//standalone Client
+		NetworkTransport.Connect(hostId, SERVER_IP, PORT, 0, out error);
+		Debug.Log(string.Format("Connecting from standalone"));
+#endif
+		
+		Debug.Log(string.Format("연결 시도하는 중 {0}...", SERVER_IP));
+		isStarted = true;
+		Debug.Log(string.Format("연결 시도 완료"));
+	}
+
+	public void Shutdown() {
+		isStarted = false;
+		NetworkTransport.Shutdown();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
---
+++ 02.Feb/MultiLoby_Server/Assets/Scripts/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour {
+
+	const int MAX_USER = 100;
+	const int PORT = 26000;
+	const int WEB_PORT = 26001;
+
+	byte reliableChannel;
+	int hostId;
+	int webHostId;
+
+	bool isStarted = false;
+
+	#region Monobehaviour
+	void Start () {
+		DontDestroyOnLoad(this.gameObject);
+		Init();
+	}
+	#endregion
+
+	public void Init() {
+		NetworkTransport.Init();
+
+		ConnectionConfig cc = new ConnectionConfig();
+		reliableChannel = cc.AddChannel(QosType.Reliable);
+
+		HostTopology topo = new HostTopology(cc, MAX_USER);
+
+		//server only code
+		hostId = NetworkTransport.AddHost(topo, PORT, null);
+		webHostId = NetworkTransport.AddWebsocketHost(topo, WEB_PORT, null);
+		Debug.Log(string.Format("호스트연결 {0}... Web {1}", PORT, WEB_PORT));
+		isStarted = true;
+	}
+
+	public void Shutdown() {
+		isStarted = false;
+		NetworkTransport.Shutdown();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
