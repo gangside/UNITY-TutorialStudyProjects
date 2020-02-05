304f5c726e360d8d13a32907cee4b8edecf5ce7a
gangside <x_hojun@naver.com>
2020-02-05 22:49:06 +0900

    Clear multilobby ep2, sending and listening

--- 02.Feb/MultiLoby_Server/Assets/Scripts/Server.cs
+++ 02.Feb/MultiLoby_Server/Assets/Scripts/Server.cs
@@ -9,10 +9,12 @@ public class Server : MonoBehaviour {
 	const int MAX_USER = 100;
 	const int PORT = 26000;
 	const int WEB_PORT = 26001;
+	const int BYTE_SIZE = 1024;
 
 	byte reliableChannel;
 	int hostId;
 	int webHostId;
+	byte error;
 
 	bool isStarted = false;
 
@@ -20,6 +22,10 @@ public class Server : MonoBehaviour {
 	void Start () {
 		DontDestroyOnLoad(this.gameObject);
 		Init();
+	}
+
+	private void Update() {
+		UpdateMessagePump();
 	}
 	#endregion
 
@@ -43,8 +49,37 @@ public class Server : MonoBehaviour {
 		NetworkTransport.Shutdown();
 	}
 
-	// Update is called once per frame
-	void Update () {
+	public void UpdateMessagePump() {
+		if (!isStarted) return;
+
+		int recHostId; // 이게 웹에서 오냐 스탠드얼론이냐?
+		int connectionId; // 유저가 이것을 나에게 보내는가
+		int channelId; // 어디서부터 메세지를 보내고 있는가;.
+
+		byte[] recBuffer = new byte[BYTE_SIZE];
+
+		int dataSize;
+		byte error;
 		
+		NetworkEventType type = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, recBuffer.Length, out dataSize, out error);
+
+		switch (type) {
+			case NetworkEventType.DataEvent:
+				Debug.Log("데이터 발생");
+				break;
+			case NetworkEventType.ConnectEvent:
+				Debug.Log(string.Format("유저 [{0}]이 로그인 했습니다.", connectionId));
+				break;
+			case NetworkEventType.DisconnectEvent:
+				Debug.Log(string.Format("유저 [{0}]이 종료 했습니다.", connectionId));
+				break;
+			case NetworkEventType.Nothing:
+				break;
+			case NetworkEventType.BroadcastEvent:
+				Debug.Log("예상되지 않은 타입입니다");
+				break;
+			default:
+				break;
+		}
 	}
 }
